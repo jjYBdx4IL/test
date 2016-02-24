a1a45046d57a67d1f87989365f8a270e831c9c7f
jjYBdx4IL <jjYBdx4IL@github.com>
2016-02-24 13:22:31 +0100

    https://bitbucket.org/CoherentLogic/coherent-logic-fred-client/issues/2/newer-jersey-common-versions-break-uri

---
+++ .travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -Eex
+set -o pipefail
+
+git clone https://Fpf6kXQg@bitbucket.org/CoherentLogic/coherent-logic-fred-client.git
+git clone https://Fpf6kXQg@bitbucket.org/CoherentLogic/coherent-logic-foundation-data-model.git
+
+sed -i coherent-logic-foundation-data-model/bom/pom.xml -e 's:>2.4.1<:>2.22.2<:g'
+pushd coherent-logic-foundation-data-model
+git diff
+popd
+mvn clean install -DskipTests -f coherent-logic-foundation-data-model
+mvn clean install -DskipTests -f coherent-logic-fred-client
+mvn install -f coherent-logic-foundation-data-model
+mvn -o test -f coherent-logic-fred-client/fred-client-core-it \
+    "-Dtest=com.coherentlogic.fred.client.core.builders.QueryBuilderTest#getAllSeries"
+
